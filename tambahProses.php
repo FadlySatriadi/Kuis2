a88533721353621e707fe4d9ffac0a4f2a2e9dbd
FadlySatriadi <fadlysatriady17@gmail.com>
2022-05-18 21:23:00 +0700

    Submit Kuis 2

---
+++ tambahProses.php
@@ -0,0 +1,25 @@
+<?php
+
+    $file = $target. basename($_FILES["name"]);
+
+    if(move_uploaded_file($_FILES["tmp_name"],$file)) {
+        $kdbuku = $_POST["kdbuku"];
+        $judul = $_POST["judulbuku"];
+        $pengarang = $_POST["nmpengarang"];
+        $penerbit = $_POST["nmpenerbit"];
+        $deskripsi = $_POST["deskripsibuku"];
+        
+        include "myconnection.php";
+        $query = "INSERT INTO buku VALUES 
+        ('$kdbuku','$judul','$pengarang','$penerbit', '$file','$deskripsi')";
+
+        if(mysqli_query($connect, $query)) {
+            header("Location:homeAdmin.php");
+        } else {
+            echo "Data baru gagal ditambahkan! <br>". mysqli_error($connect);
+        }
+        mysqli_close($connect);
+    } else {
+        echo "Error";
+    }
+?>
